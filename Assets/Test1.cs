599772479e37cb2a57149527dd05567fa90b191f
狂飙 <networm@live.com>
2023-11-21 19:41:23 +0800

    Add issue reproduce sample

---
+++ Assets/Test1.cs
@@ -0,0 +1,23 @@
+﻿using PrimeTween;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    private int _frame;
+
+    void Start()
+    {
+        var seq = Sequence.Create();
+        seq.Group(Tween.LocalPositionX(transform, 0.6f, 0.1f));
+        seq.SetCycles(10);
+    }
+
+    void Update()
+    {
+        _frame++;
+        if (_frame == 120)
+        {
+            Tween.StopAll(transform);
+        }
+    }
+}
